5caf9f43938ca3c37d6254424d630d816cb618f6
Дмитро Фесак <Fesak.D.V@nmu.one>
2021-03-23 17:55:52 +0200

    Updated interface IServerLogic

--- AgeChatServer/IServerLogic.cs
+++ AgeChatServer/IServerLogic.cs
@@ -9,6 +9,12 @@ namespace AgeChatServer
     interface IServerLogic
     {
         void run();
-
+        void request(string method);
+        void login(string log, string pass);
+        void registration(string log, string pass, string username);
+        void messageToGlobalChat(string msg, Client sender);
+        void personalMessage(string msg, Client sender, Client receiver);
+        void sendGlobalMessageList(Client client);
+        void sendMessageList(Client receiver, Client sender);
     }
 }
