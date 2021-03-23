66323803d67e1397b87834ad23580567f1b14417
Дмитро Фесак <Fesak.D.V@nmu.one>
2021-03-23 17:21:16 +0200

    added Opcode

--- AgeChatServer/IServerLogic.cs
+++ AgeChatServer/IServerLogic.cs
@@ -9,5 +9,6 @@ namespace AgeChatServer
     interface IServerLogic
     {
         void run();
+
     }
 }
---
+++ AgeChatServer/Opcode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeChatServer
+{
+    static class Opcode
+    {
+        static int Fragment = 0;
+        static int Text = 1;
+        static int Binary = 2;
+        static int CloseConnection = 8;
+        static int Ping = 9;
+        static int Pong = 10;
+    }
+}
