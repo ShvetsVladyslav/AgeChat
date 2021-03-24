e12abb2cc7a4eaba0d8bf03b225fce8e7333d3a8
Дмитро Фесак <Fesak.D.V@nmu.one>
2021-03-24 15:04:13 +0200

    improved code

--- AgeChatServer/Server.cs
+++ AgeChatServer/Server.cs
@@ -18,8 +18,6 @@ namespace AgeChatServer
         List<Client> connectedClients;
         public void Run()
         {
-            var server = new Server();
-
             var listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             listeningSocket.Bind(new IPEndPoint(IPAddress.Any, port: 8080));
             listeningSocket.Listen(0);
