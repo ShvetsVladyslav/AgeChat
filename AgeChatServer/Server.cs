4086847c3a21580fe8aec0c25d40dc7fc1cc9fa0
Дмитро Фесак <Fesak.D.V@nmu.one>
2021-03-26 18:16:51 +0200

    improved logout

--- AgeChatServer/Server.cs
+++ AgeChatServer/Server.cs
@@ -97,6 +97,7 @@ namespace AgeChatServer
             {
                 GoOffline(client);
                 client.DisconnectUser();
+                Console.WriteLine("client logged out");
             }
         }
 
