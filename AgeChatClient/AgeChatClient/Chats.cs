bb52712c80ebab4773192ad2e175825bea2457bd
Дмитро Фесак <Fesak.D.V@nmu.one>
2021-05-07 17:19:42 +0300

    improved chats page

--- AgeChatClient/AgeChatClient/Chats.cs
+++ AgeChatClient/AgeChatClient/Chats.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Threading;
 using WebSocket4Net;
+using Xamarin.Forms;
 
 namespace AgeChatClient
 {
@@ -10,10 +12,11 @@ namespace AgeChatClient
     {
         WebSocket ws;
         List<string> messages;
-        string[] users;
+        ObservableCollection<User> users;
         public Chats(WebSocket ws)
         {
             messages = new List<string>();
+            users = new ObservableCollection<User>();
             this.ws = ws;
             ws.MessageReceived += ReceivedMessage;
             GetUsers();
@@ -28,23 +31,60 @@ namespace AgeChatClient
         {
             ws.Send("showAll");
             Thread.Sleep(100);
-            users = new string[messages.Count];
             for (int i = 0; i < messages.Count; i++)
             {
-                users[i] = messages[i];
+                users.Add(new User(messages[i]));
             }
             messages.Clear();
             ws.Send("showOn");
             Thread.Sleep(100);
+            for (int i = 0; i < users.Count; i++)
+            {
+                for (int j = 0; j < messages.Count; j++)
+                {
+                    if (users[i].username == messages[j])
+                    {
+                        users[i].color = Color.LightGreen;
+                        messages.RemoveAt(j);
+                        break;
+                    }
+                }
+            }
+            User tmp;
+            for (int i = 1; i < users.Count; i++)
+            {
+                if (users[i].color == Color.LightGreen)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (users[j].color == Color.LightGray)
+                        {
+                            tmp = users[j];
+                            users[j] = users[i];
+                            users[i] = tmp;
+                        }
+                    }
+                }
+            }
         }
         
-        public string[] userCollection 
+        public ObservableCollection<User> userCollection 
         {
             get
             {
                 return users;
             }
         }
+    }
 
+    class User
+    {
+        public User(string username)
+        {
+            this.username = username;
+            color = Color.LightGray;
+        }
+        public string username{get; set;}
+        public Color color{get; set;}
     }
 }
